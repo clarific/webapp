04a7807fc9cff39907d04b5c1ae7032f993ce1e1
Rodrigo V. Cortezi <rodrigovcortezi@gmail.com>
2022-06-02 19:09:42 -0300

    Edit website social card

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,32 +1,49 @@
-const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === 'production';
+const baseUrl = isProd ? 'clarific.com.br' : 'localhost:3000';
 
 export default {
   mode: 'universal',
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
     title: 'Design e desenvolvimento de produtos digitais',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      },
+      { hid: 'og-type', property: 'og:type', content: 'website' },
+      { hid: 'og-title', property: 'og:title', content: 'Clarific' },
+      {
+        hid: 'og-desc',
+        property: 'og:description',
+        content: 'Design e desenvolvimento de produtos digitais'
+      },
+      {
+        hid: 'og-image',
+        property: 'og:image',
+        content: 'https://clarific.com.br/clarific.png'
+      }
     ]
   },
   /*
-  ** Customize the progress-bar color
-  */
+   ** Customize the progress-bar color
+   */
   loading: { color: '#8E54E9' },
   /*
-  ** Global CSS
-  */
+   ** Global CSS
+   */
   css: [
     // TODO: Alterar custom.scss para tratar home e lps
     '@/assets/scss/custom.scss'
   ],
   /*
-  ** Plugins to load before mounting the App
-  */
+   ** Plugins to load before mounting the App
+   */
   plugins: [
     '~/plugins/vuelidate.js',
     { src: '~/plugins/validate', mode: 'client' },
@@ -34,18 +51,16 @@ export default {
     '~/plugins/mask.js'
   ],
   /*
-  ** Server Middlewares
-  */
+   ** Server Middlewares
+   */
   serverMiddleware: [],
   /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-    '@nuxtjs/gtm'
-  ],
+   ** Nuxt.js dev-modules
+   */
+  buildModules: ['@nuxtjs/gtm'],
   /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js modules
+   */
   modules: [
     'nuxt-buefy',
     '@nuxtjs/axios',
@@ -53,30 +68,29 @@ export default {
     ['vue-scrollto/nuxt', { container: 'html' }]
   ],
   /*
-  ** Axios module configuration
-  ** See https://axios.nuxtjs.org/options
-  */
+   ** Axios module configuration
+   ** See https://axios.nuxtjs.org/options
+   */
   axios: {
     https: true,
-    baseUrl: isProd ? 'clarific.com.br' : 'localhost:3000',
+    baseUrl,
     browserBaseURL: '/',
     progress: false
   },
   /*
-  ** Google Tag Manager config
-  */
+   ** Google Tag Manager config
+   */
   gtm: {
     id: 'GTM-5SKDTQ5',
     pageTracking: true
   },
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
-    extend (config, ctx) {
-    }
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
   }
-}
+};
